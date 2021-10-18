e1e334dbb6b465e5379954450f86d2267f13b2ae
Vladislav <Fommkaa2304@mail.ru>
2021-10-18 13:53:08 +0300

    Added some entities

---
+++ src/main/java/com/dimevision/userserviceapplication/domain/User.java
@@ -0,0 +1,34 @@
+package com.dimevision.userserviceapplication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
